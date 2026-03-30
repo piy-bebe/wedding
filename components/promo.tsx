c997b79d85265da692bf678a049538f12c2ac316
piy-bebe <piy.bebe@gmail.com>
2026-03-30 13:34:02 +0300

    feat: добавить переход с открытки

--- components/promo.tsx
+++ components/promo.tsx
@@ -1,15 +1,29 @@
+'use client';
+import { useState } from 'react';
 import styles from '@/styles/promo.module.scss';
 import Image from 'next/image';
 
 export default function Promo() {
+  const [hiding, setHiding] = useState(false);
+  const [hidden, setHidden] = useState(false);
+
+  const handleClose = () => {
+    setHiding(true);
+    setTimeout(() => setHidden(true), 800);
+  };
+
+  if (hidden) return null;
+
   return (
-    <div className={styles.promo}>
+    <div className={`${styles.promo} ${hiding ? styles.hiding : ''}`}>
       <div className={styles.wrapper}>
         <div className={styles.container}>
           <Image src="/flower.svg" alt="flower" width={127} height={127} loading="eager" priority />
           <p className={styles.text}>Приглашение на свадьбу</p>
           <h1 className={styles.title}>Виктор и Светлана</h1>
-          <button className={styles.button}>Открыть</button>
+          <button className={styles.button} onClick={handleClose}>
+            Открыть
+          </button>
         </div>
       </div>
     </div>
